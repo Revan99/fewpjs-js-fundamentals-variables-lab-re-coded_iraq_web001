5ef3cd99b0f839da0feb46978797ff7ec94d398f
Revan Sarbast <revansarbast8@gmail.com>
2020-05-13 10:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const companyName = 'Scuber'
 let mostProfitableNeighborhood = 'Chelsea'
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith'
+=======
+let companyCeo;
+>>>>>>> 2b6d705c7fb2e2807c7b4b9a2e27d20a87c8c19f
